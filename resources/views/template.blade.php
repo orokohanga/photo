75529815f1b31ca6f5fa2d7415f9169da15f9bae
Delmote Axel <delmote.axel@gmail.com>
2023-12-05 15:05:20 +0100

    Master2

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -6,7 +6,22 @@
 	<link rel="stylesheet" type="text/css" href="/css/style.css" />
 	</head>
 	<body>
-	<header>Ma super application</header>
+        <header>
+            <div class="header">
+            <h3>Accueil</h3>
+            <div class="search">
+                <a href="#">
+                <i class='bx bx-search-alt-2'></i>
+            </a>
+                <input type="search" placeholder="Rechercher">
+                <a>
+                <i class='bx bx-eraser'></i>
+                </a>
+    
+            </div>
+            <h3>Profile</h3>
+            </div>
+        </header>
 	<nav>
 	    <a href="/albums">Liste des albums</a>
         <a href="/explorer">explorer</a>
